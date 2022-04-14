545827c53d3fca63cce6e9f85f01f57a86d55c9c
gkrgry <79895925+gkrgry@users.noreply.github.com>
2022-04-14 18:19:57 +0900

    리스트 조회시 없을 경우 모달 창

--- src/main/java/com/daelim/clover/board/controller/BoardController.java
+++ src/main/java/com/daelim/clover/board/controller/BoardController.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Log4j2
@@ -47,7 +51,7 @@ public class BoardController {
         int total = boardService.mapSearchlistGetTotal(cri);
         total = total + 10;
         log.info(total);
-
+        model.addAttribute("total",total);
         model.addAttribute("boardList", boardMapSearchList);
         model.addAttribute("pageMaker",new PageDTO(cri,total));
 
@@ -61,7 +65,7 @@ public class BoardController {
         int total = boardService.listGetTotal(cri);
         total = total + 10;
         log.info(total);
-
+        model.addAttribute("total",total);
         model.addAttribute("boardList", boardList);
         model.addAttribute("pageMaker",new PageDTO(cri,total));
         return "bList";
@@ -83,14 +87,47 @@ public class BoardController {
         boardService.boardRegister(board);
         model.addAttribute("msg", "입력 성공했스니다.");
 
-        return "success";
+        return "main";
     }
     @GetMapping("/read")
-    public String boardRead(@RequestParam("boardId") int boardId, Model model) throws Exception{
+    public String boardRead(@RequestParam("boardId") int boardId, Model model, HttpServletRequest request,
+                            HttpServletResponse response, HttpSession session) throws Exception{
+        Cookie oldCookie = null;//비교하기 위한 쿠기
+        Cookie[] cookies = request.getCookies();
         log.info("read 입력 입니다.");
+
+
         Board board = boardService.boardRead(boardId);
-        int boardCountSet = boardService.boardCountSet(boardId);
-        model.addAttribute("boardCountSet", boardCountSet);
+
+        //쿠키가 있을경우
+        if(cookies != null && cookies.length > 0){
+            for(int i=0; i< cookies.length; i++){
+                // cookie의 name이 cookie + boardId와 일치하는 쿠키를 oldCookie에 넣어줌
+                if(cookies[i].getName().equals("cookie"+boardId)){
+                    log.info("쿠키 처음 생성한 뒤 돌아옴");
+                    oldCookie = cookies[i];
+                }
+            }
+        }
+
+
+        if(oldCookie == null) {
+            //쿠키 생성
+            Cookie newCookie = new Cookie("cookie" + boardId, "|" + boardId + "|");
+            //쿠키 추가
+            response.addCookie(newCookie);
+
+            //쿠키를 추가 시키고 조회수를 증가
+            int boardCountSet = boardService.boardCountSet(boardId);//조회수 증가
+            model.addAttribute("boardCountSet", boardCountSet);
+
+        }else { //oldCookie 가 null 아닌거 즉 쿠기가 있으므로 조회수 증가 로직을 처리 X
+            String value = oldCookie.getValue();
+            log.info(value);
+        }
+
+
+
         model.addAttribute("board",board);
 
 
@@ -117,7 +154,7 @@ public class BoardController {
 
         model.addAttribute("msg","수정 성공");
 
-        return "success";
+        return "main";
     }
 
 
@@ -131,6 +168,9 @@ public class BoardController {
         boardService.boardRemove(boardId,indexId);
 
         model.addAttribute("msg","삭제 성공");
-        return "success";
+        return "main";
     }
+
+
+
 }

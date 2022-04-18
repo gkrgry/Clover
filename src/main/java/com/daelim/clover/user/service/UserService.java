d87b10000ab9851b20c00921ecb2a63e64048e5b
김호준 <rlaghwns126@naver.com>
2022-04-18 22:10:20 +0900

    이메일 인증 구현 이메일 중복 체크 알림 설정 추후 부트스트랩으로 디자인 해야함

--- src/main/java/com/daelim/clover/user/service/UserService.java
+++ src/main/java/com/daelim/clover/user/service/UserService.java
@@ -6,9 +6,17 @@ import com.daelim.clover.user.domain.User;
 
 public interface UserService {
 
+
+
     //회원가입
     public void userSingUp(User user) throws  Exception;
 
     //아이디 중복확인
     public int idCheck(String userId) throws Exception;
+
+    //이메일 인증
+    String sendSimpleMessage(String to)throws  Exception;
+
+    //이메일 중복확인
+    public int emailCheck(String email)throws Exception;
 }
